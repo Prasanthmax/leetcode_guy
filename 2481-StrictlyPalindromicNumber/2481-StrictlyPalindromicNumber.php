38496a4c6defb6363dc53cc7dd3acd6c3ca0bc9d
Prasanth S <142437996+Prasanthmax@users.noreply.github.com>
2026-07-14 14:44:52 +0530

    Create 2481-StrictlyPalindromicNumber

---
+++ 2481-StrictlyPalindromicNumber/2481-StrictlyPalindromicNumber.php
@@ -0,0 +1,11 @@
+// Last updated: 14/07/2026, 14:44:44
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isStrictlyPalindromic($n) {
+        return false;
+    }
+}
