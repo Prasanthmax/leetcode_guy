1a563ef65dc020401b61848c1eb9992a5a34dc85
Prasanth S <142437996+Prasanthmax@users.noreply.github.com>
2026-07-14 14:47:23 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ 448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,18 @@
+// Last updated: 14/07/2026, 14:47:16
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] = -nums[num - 1];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1); 
+            }
+        }
+        return result;
+    }
+}
