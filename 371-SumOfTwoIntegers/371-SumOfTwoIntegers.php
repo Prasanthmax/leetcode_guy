f8b45bc86c3707786ab846be4d5e6cf4d7f66653
Prasanth S <142437996+Prasanthmax@users.noreply.github.com>
2026-07-14 14:49:00 +0530

    Create 371-SumOfTwoIntegers

---
+++ 371-SumOfTwoIntegers/371-SumOfTwoIntegers.php
@@ -0,0 +1,12 @@
+// Last updated: 14/07/2026, 14:48:53
+class Solution {
+
+    /**
+     * @param Integer $a
+     * @param Integer $b
+     * @return Integer
+     */
+    function getSum($a, $b) {
+        return $a+$b;
+    }
+}
