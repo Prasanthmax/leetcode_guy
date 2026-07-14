e3eee962cc8bfb187a52635338f977a3009fd5c4
Prasanth S <142437996+Prasanthmax@users.noreply.github.com>
2026-07-14 14:45:39 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.php
@@ -0,0 +1,12 @@
+// Last updated: 14/07/2026, 14:45:32
+class Solution {
+
+    /**
+     * @param Integer $num1
+     * @param Integer $num2
+     * @return Integer
+     */
+    function sum($num1, $num2) {
+        return $num1+$num2;
+    }
+}
