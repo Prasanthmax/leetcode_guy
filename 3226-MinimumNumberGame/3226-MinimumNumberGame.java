cb7e0639728ef733ef66e5e0a61ed55d23bd6c33
Prasanth S <142437996+Prasanthmax@users.noreply.github.com>
2026-07-14 14:43:19 +0530

    Create 3226-MinimumNumberGame

---
+++ 3226-MinimumNumberGame/3226-MinimumNumberGame.java
@@ -0,0 +1,12 @@
+// Last updated: 14/07/2026, 14:43:12
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int[] arr=new int[nums.length];
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=2){
+            arr[i]=nums[i+1];
+            arr[i+1]=nums[i];
+        }
+        return arr;
+    }
+}
